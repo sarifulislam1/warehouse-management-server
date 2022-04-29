b0f31822b4761c807031d304431501a45f3b1674
Your Name <sarifulislaml8280@gmail.com>
2022-04-30 03:19:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// sariful
+// EKLGR6ar55WFnzUB
+
+const express = require("express")
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
